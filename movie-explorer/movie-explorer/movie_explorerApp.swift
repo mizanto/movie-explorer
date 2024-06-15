866447247c8141b5d5ceb5a75cd1ee6d41636a1c
Sergey Bendak <sleepoke@gmail.com>
2024-06-15 12:34:21 +0300

    add empty ios project

---
+++ movie-explorer/movie-explorer/movie_explorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_explorerApp.swift
+//  movie-explorer
+//
+//  Created by Sergey Bendak on 15.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct movie_explorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
