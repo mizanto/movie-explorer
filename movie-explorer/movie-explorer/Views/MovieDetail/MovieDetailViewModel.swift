f03cfc4b7f86ccbae20bad743103ee2de7d4df96
Sergey Bendak <sleepoke@gmail.com>
2024-06-21 16:09:28 +0300

    update MovieDetailViewModel

--- movie-explorer/movie-explorer/Views/MovieDetail/MovieDetailViewModel.swift
+++ movie-explorer/movie-explorer/Views/MovieDetail/MovieDetailViewModel.swift
@@ -9,23 +9,45 @@ import Foundation
 import Combine
 
 class MovieDetailViewModel: ObservableObject {
-    @Published var movie: MovieDetail
+    @Published var model: MovieDetailView.Model
     @Published var errorMessage: String? = nil
 
     private let client: ClientProtocol
+    
+    private let movie: Movie
 
     init(client: ClientProtocol, movie: Movie) {
         self.client = client
-        self.movie = MovieDetail(from: movie)
+        self.movie = movie
+        self.model = MovieDetailView.Model(imagePath: movie.backdropPath,
+                                           title: movie.title,
+                                           releaseDate: nil,
+                                           genres: nil,
+                                           rating: String(format: "%.1f", movie.voteAverage),
+                                           overview: nil)
     }
 
     @MainActor
     func loadMovieDetail() async {
         do {
             let movieDetail = try await client.getMovieDetail(id: movie.id)
-            self.movie = movieDetail
+            self.model = MovieDetailView.Model(
+                imagePath: movieDetail.backdropPath,
+                title: movieDetail.title,
+                releaseDate: movieDetail.releaseDate,
+                genres: movieDetail.genres?.joined(separator: ", "),
+                rating: rating(average: movieDetail.voteAverage, count: movieDetail.voteCount),
+                overview: movieDetail.overview)
         } catch {
             self.errorMessage = error.localizedDescription
+        }
+    }
+    
+    private func rating(average: Double, count: Int? = nil) -> String {
+        if let count = count {
+            String(format: "%.1f (%d votes)", average, count)
+        } else {
+            String(format: "%.1f", average)
         }
     }
 }
